109a0d9f0f0b52ad7d5ec06619c48fb6e74fe468
Ithihas_Ballal_p <ithihasballalp@gmail.com>
2025-05-12 16:52:39 +0530

    Routes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.get("/user", (req,res) =>{
+    res.send({firstName:"Ithihas",lateName:"Ballal"})
+});
+
+app.post("/user",(req,res)=>{
+    console.log("Save data to the database");
+    res.send("Data successfully saved to the database!!!")
+});
+
+app.delete("/user",(req,res) =>{
+    res.send("data deleted successfully...!!")
+});
+
 app.use('/hello',(req,res)=>{
     res.send("Nameskar Ithihas")
 });
